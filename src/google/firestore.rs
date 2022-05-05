f16961b2490f381f68d5e6d0f078a8abb48d0adf
okinari <ooba@okinari.com>
2022-05-05 18:33:48 +0900

    jsonの形式をfirestore用の自動で変換してくれるように調整

--- src/google/firestore.rs
+++ src/google/firestore.rs
@@ -76,39 +76,48 @@ impl GoogleFirestore {
 		headers.insert("Accept".to_string(), "application/json".to_string());
 		headers.insert("Authorization".to_string(), self.get_access_token());
 
-		http::post_json(url, Some(headers), params).await
+		let body_json: Value = serde_json::from_str(&generate_object(&params)).ok().unwrap();
+		http::post_json(url, Some(headers), body_json).await
 	}
 }
 
-fn generate_params(params: Value) -> String {
-
+fn generate_values(params: &Value) -> String {
 	if params.is_array() {
-
+		let tmp = params.as_array().unwrap();
+		let mut vec = vec![];
+		for val in tmp {
+			vec.push(format!("{}", generate_values(val)));
+		}
+		return format!("{{\"arrayValue\": {{\"values\": [ {} ] }} }}", vec.join(","));
 	}
 	else if params.is_boolean() {
-
+		return format!("{{\"booleanValue\": {} }}", params.as_bool().unwrap());
 	}
 	else if params.is_f64() {
-
+		return format!("{{\"doubleValue\": {} }}", params.as_f64().unwrap());
 	}
 	else if params.is_i64() {
-
+		return format!("{{\"integerValue\": {} }}", params.as_i64().unwrap());
 	}
 	else if params.is_null() {
-
-	}
-	else if params.is_number() {
-
+		return String::from("{{\"nullValue\": \"NULL_VALUE\"}}");
 	}
 	else if params.is_object() {
-
+		return format!("{{\"mapValue\": {} }}", generate_object(params));
 	}
 	else if params.is_string() {
-
-	}
-	else {
-		// error
+		return format!("{{\"stringValue\": \"{}\" }}", params.as_str().unwrap());
 	}
 
+	// error
 	"".to_string()
+}
+
+fn generate_object(params: &Value) -> String {
+	let tmp = params.as_object().unwrap();
+	let mut vec = vec![];
+	for( key, val) in tmp.iter() {
+		vec.push(format!("\"{}\": {}", key, generate_values(val)));
+	}
+	format!("{{\"fields\": {{ {} }} }}", vec.join(","))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod utils;
 mod google;
 
-use actix_web::{get, post, web, App, HttpServer, HttpResponse, Responder};
+use actix_web::{web, App, HttpServer, HttpResponse, Responder};
 use serde::{Deserialize, Serialize};
 use config::Config;
 
@@ -16,12 +16,12 @@ struct Personal {
 	tel: String,
 }
 
-#[get("/{id}/{name}/index.html")]
+// #[get("/{id}/{name}/index.html")]
 async fn index(web::Path((id, name)): web::Path<(u32, String)>) -> impl Responder {
 	format!("Hello {}! id:{}", name, id)
 }
 
-#[get("/list")]
+// #[get("/list")]
 async fn list() -> impl Responder {
 	let firestore = setup_firestore().await;
 	
@@ -31,36 +31,58 @@ async fn list() -> impl Responder {
 	HttpResponse::Ok().body("ok")
 }
 
-#[post("/enter")]
+// #[post("/enter")]
 async fn enter(personal_by_web_json: web::Json<Personal>) -> impl Responder {
 	println!("post_data: {:?}", personal_by_web_json);
 
 	let firestore = setup_firestore().await;
 
 	let enter_data = json!({
-		"fields": {
-			"name": {
-				"stringValue": personal_by_web_json.name,
+		"num": 89,
+		"float": 72.3,
+		"array": [
+			"string",
+			2,
+			43.9,
+			true,
+			false,
+			{
+				"key1str": "value1",
+				"key2num": 44,
+				"key3float": 65.9,
+				"key4true": true,
+				"key5false": false,
+				"key6obj": {
+					"key1": "key2",
+					"key6obj": {
+						"key1": "key2",
+					},
+				},
 			},
-			"tel": {
-				"stringValue": personal_by_web_json.tel,
+		],
+		"map": {
+			"key1": "value1",
+			"key2num": 44,
+			"key3float": 65.9,
+			"key4true": true,
+			"key5false": false,
+			"key6obj": {
+				"key1": "key2",
+				"key6obj": {
+					"key1": "key2",
+				},
 			},
 		},
+		"bool1": true,
+		"bool2": false,
+		"name": personal_by_web_json.name,
+		"tel": personal_by_web_json.tel,
 	});
 
 	let response = firestore.insert_document("tests/bbb/sss".to_string(), None, enter_data).await;
 	println!("response: {:#?}", response);
 
 	HttpResponse::Ok().body("ok")
-}
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-	HttpServer::new(|| App::new()
-		.service(list))
-		.bind("127.0.0.1:8080")?
-		.run()
-		.await
 }
 
 fn get_configs() -> Config {
@@ -84,4 +106,18 @@ async fn setup_firestore() -> GoogleFirestore {
 		google_credential.get_project_id().to_string(),
 		"(default)".to_string(),
 	)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+	HttpServer::new(||{
+		App::new()
+			.route("/", web::get().to(index))
+			.route("/list", web::get().to(list))
+			.route("/enter", web::post().to(enter))
+	})
+	//.service(list))
+	.bind("127.0.0.1:8080")?
+	.run()
+	.await
 }
